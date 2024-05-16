45f6dafb7571ebfae1b7e307070cbedb39c42ca6
danielleurquin <danielleurquin@hotmail.fr>
2024-05-16 14:27:35 +0200

    project init

---
+++ src/main/java/com/isep/tsn/TsnBackApplication.java
@@ -0,0 +1,13 @@
+package com.isep.tsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsnBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsnBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isep/tsn/TsnBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isep.tsn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsnBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
